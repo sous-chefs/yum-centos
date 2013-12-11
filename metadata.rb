42adfae327478f6b769df2edfcf983f828109b9c
Sean OMeara <someara@opscode.com>
2013-12-11 13:47:19 -0500

    pinning yum dep to ~> 3.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'yum-centos'
 maintainer       'Chef'
-maintainer_email 'Sean OMeara <someara@opscode.com>'
+maintainer_email 'Sean OMeara <someara@getchef.com>'
 license          'Apache 2.0'
 description      'Installs/Configures yum-centos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'yum'
+depends 'yum', '~> 3.0'
