814f747a3571b5101c4475bd3645eb981baecfaa
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:07:43 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,12 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-centos'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.6'
 
 depends 'yum', '~> 3.2'
+
+supports 'centos'
+
+source_url 'https://github.com/chef-cookbooks/yum-centos' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-centos/issues' if respond_to?(:issues_url)
