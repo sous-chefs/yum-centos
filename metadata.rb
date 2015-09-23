9ca85fc86957134efe8b72100ee9f37b9d2eb042
Tim Smith <tsmith84@gmail.com>
2015-09-22 23:34:49 -0700

    Update the description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'yum-centos'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-centos'
+description 'Installs and configures the centos Yum repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.6'
 
