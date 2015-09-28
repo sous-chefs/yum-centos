8ae82c77ef2ad33d2bee8a53235a59705e80dda4
Stewart, Jeremy <JStewart@cvent.com>
2015-09-28 13:18:16 -0400

    NA-0: Fixes an issue where running on a system where platform=redhat would cause a nil exception

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,1 @@
-default['yum-centos']['repos'] = %w(base updates extras centosplus fasttrack) << (value_for_platform(centos: {
-                                                                                                       '>= 7.0' => 'cr',
-                                                                                                       :default => 'contrib'
-                                                                                                     }))
+default['yum-centos']['repos'] = %w(base updates extras centosplus fasttrack) << (value_for_platform( %w(centos redhat) => { '>= 7.0' => 'cr', :default => 'contrib' }))
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the centos Yum repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.7'
+version '0.4.8'
 
 depends 'yum', '~> 3.2'
 
