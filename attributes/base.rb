c0cb46d195f853f33c2d90e725c5bb80d051497b
Tim Smith <tsmith84@gmail.com>
2015-12-07 10:39:11 -0800

    Avoid a case statement to determine the GPG key file

--- attributes/base.rb
+++ attributes/base.rb
@@ -4,11 +4,4 @@ default['yum']['base']['description'] = 'CentOS-$releasever - Base'
 default['yum']['base']['enabled'] = true
 default['yum']['base']['managed'] = true
 default['yum']['base']['gpgcheck'] = true
-case node['platform_version'].to_i
-when 5
-  default['yum']['base']['gpgkey'] = 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5'
-when 6
-  default['yum']['base']['gpgkey'] = 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6'
-when 7
-  default['yum']['base']['gpgkey'] = 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7'
-end
+default['yum']['base']['gpgkey'] = 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-$releasever'
