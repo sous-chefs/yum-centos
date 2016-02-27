101b7a912ca2adb19e506cf4038b651a23eb443f
Sean OMeara <sean@chef.io>
2016-02-26 21:52:57 -0500

    grumble grumble rubocop

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['yum-centos']['repos'] = %w(base updates extras centosplus fasttrack) << (value_for_platform(%w(centos redhat) => {
-                                                                                                       '>= 7.0' => 'cr',
-                                                                                                       :default => 'contrib'
-                                                                                                     }))
+default['yum-centos']['repos'] = %w(base updates extras centosplus fasttrack) << value_for_platform(%w(centos redhat) => {
+                                                                                                      '>= 7.0' => 'cr',
+                                                                                                      :default => 'contrib'
+                                                                                                    })
