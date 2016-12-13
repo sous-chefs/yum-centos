5b7858f6d7e7d942a493fd9e60585e758ec86ace
James Le Cuirot <james.le-cuirot@yakara.com>
2016-12-13 11:25:38 +0000

    XenServer is sufficiently like CentOS to use contrib/cr repositories
    Signed-off-by: James Le Cuirot <james.le-cuirot@yakara.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-contrib = value_for_platform(%w(centos redhat) => {
+contrib = value_for_platform(%w(centos redhat xenserver) => {
                                '>= 7.0' => 'cr',
                                :default => 'contrib',
                              })
