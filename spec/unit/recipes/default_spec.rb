e31fa5eec75b8489600bc2f7803a08e31bc84485
Tim Smith <tsmith@chef.io>
2017-06-15 11:17:30 -0700

    Include a useful chefspec
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe 'yum-centos::default' do
-  let(:chef_run) do
-    ChefSpec::SoloRunner.converge(described_recipe)
-  end
+  cached(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-  it 'converges successfully' do
-    expect { :chef_run }.to_not raise_error
+  it 'creates yum_repository[base]' do
+    expect(chef_run).to create_yum_repository('base')
+      .with(mirrorlist: 'http://mirrorlist.centos.org/?release=7&arch=$basearch&repo=os')
+  end
+  it 'creates yum_repository[updates]' do
+    expect(chef_run).to create_yum_repository('updates')
+      .with(mirrorlist: 'http://mirrorlist.centos.org/?release=7&arch=$basearch&repo=updates')
+  end
+  it 'creates yum_repository[extras]' do
+    expect(chef_run).to create_yum_repository('extras')
+      .with(mirrorlist: 'http://mirrorlist.centos.org/?release=7&arch=$basearch&repo=extras')
   end
 end
